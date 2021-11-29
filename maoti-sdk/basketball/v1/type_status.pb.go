ff0c899e17f2844dfcf390a3ff2bfd2a0d5dcb4a
root <root@localhost.localdomain>
2021-11-29 11:00:16 +0800

    test

--- maoti-sdk/basketball/v1/type_status.pb.go
+++ maoti-sdk/basketball/v1/type_status.pb.go
@@ -20,7 +20,6 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-//
 type TypeStatusRequest struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
