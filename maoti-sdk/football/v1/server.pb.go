f9270fee921c89c5ac93bfd1bdc9f632ec90b805
root <root@localhost.localdomain>
2021-11-29 13:18:15 +0800

    test

--- maoti-sdk/football/v1/server.pb.go
+++ maoti-sdk/football/v1/server.pb.go
@@ -206,12 +206,13 @@ var file_server_proto_rawDesc = []byte{
 	0x3a, 0x0a, 0x12, 0x44, 0x61, 0x74, 0x61, 0x43, 0x65, 0x6e, 0x74, 0x65, 0x72, 0x52, 0x65, 0x73,
 	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x10, 0x0a, 0x03, 0x72, 0x65, 0x74, 0x18, 0x01, 0x20, 0x01,
 	0x28, 0x03, 0x52, 0x03, 0x72, 0x65, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18,
-	0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x32, 0x40, 0x0a, 0x08, 0x46,
-	0x6f, 0x6f, 0x74, 0x42, 0x61, 0x6c, 0x6c, 0x12, 0x34, 0x0a, 0x12, 0x4d, 0x61, 0x74, 0x63, 0x68,
-	0x41, 0x67, 0x61, 0x69, 0x6e, 0x73, 0x74, 0x44, 0x65, 0x74, 0x61, 0x69, 0x6c, 0x12, 0x11, 0x2e,
-	0x4d, 0x61, 0x74, 0x63, 0x68, 0x49, 0x6e, 0x66, 0x6f, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
-	0x1a, 0x09, 0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x07, 0x5a,
-	0x05, 0x2e, 0x2f, 0x3b, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x32, 0x48, 0x0a, 0x08, 0x46,
+	0x6f, 0x6f, 0x74, 0x42, 0x61, 0x6c, 0x6c, 0x12, 0x3c, 0x0a, 0x16, 0x46, 0x6f, 0x6f, 0x74, 0x4d,
+	0x61, 0x74, 0x63, 0x68, 0x41, 0x67, 0x61, 0x69, 0x6e, 0x73, 0x74, 0x44, 0x65, 0x74, 0x61, 0x69,
+	0x6c, 0x12, 0x15, 0x2e, 0x46, 0x6f, 0x6f, 0x74, 0x4d, 0x61, 0x74, 0x63, 0x68, 0x49, 0x6e, 0x66,
+	0x6f, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x09, 0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f,
+	0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2f, 0x3b, 0x76, 0x31, 0x62, 0x06,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -228,14 +229,14 @@ func file_server_proto_rawDescGZIP() []byte {
 
 var file_server_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_server_proto_goTypes = []interface{}{
-	(*Request)(nil),            // 0: Request
-	(*Response)(nil),           // 1: Response
-	(*DataCenterResponse)(nil), // 2: DataCenterResponse
-	(*MatchInfoRequest)(nil),   // 3: MatchInfoRequest
+	(*Request)(nil),              // 0: Request
+	(*Response)(nil),             // 1: Response
+	(*DataCenterResponse)(nil),   // 2: DataCenterResponse
+	(*FootMatchInfoRequest)(nil), // 3: FootMatchInfoRequest
 }
 var file_server_proto_depIdxs = []int32{
-	3, // 0: FootBall.MatchAgainstDetail:input_type -> MatchInfoRequest
-	1, // 1: FootBall.MatchAgainstDetail:output_type -> Response
+	3, // 0: FootBall.FootMatchAgainstDetail:input_type -> FootMatchInfoRequest
+	1, // 1: FootBall.FootMatchAgainstDetail:output_type -> Response
 	1, // [1:2] is the sub-list for method output_type
 	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -320,7 +321,7 @@ const _ = grpc.SupportPackageIsVersion6
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type FootBallClient interface {
 	//比赛对阵详情
-	MatchAgainstDetail(ctx context.Context, in *MatchInfoRequest, opts ...grpc.CallOption) (*Response, error)
+	FootMatchAgainstDetail(ctx context.Context, in *FootMatchInfoRequest, opts ...grpc.CallOption) (*Response, error)
 }
 
 type footBallClient struct {
@@ -331,9 +332,9 @@ func NewFootBallClient(cc grpc.ClientConnInterface) FootBallClient {
 	return &footBallClient{cc}
 }
 
-func (c *footBallClient) MatchAgainstDetail(ctx context.Context, in *MatchInfoRequest, opts ...grpc.CallOption) (*Response, error) {
+func (c *footBallClient) FootMatchAgainstDetail(ctx context.Context, in *FootMatchInfoRequest, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
-	err := c.cc.Invoke(ctx, "/FootBall/MatchAgainstDetail", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/FootBall/FootMatchAgainstDetail", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -343,35 +344,35 @@ func (c *footBallClient) MatchAgainstDetail(ctx context.Context, in *MatchInfoRe
 // FootBallServer is the server API for FootBall service.
 type FootBallServer interface {
 	//比赛对阵详情
-	MatchAgainstDetail(context.Context, *MatchInfoRequest) (*Response, error)
+	FootMatchAgainstDetail(context.Context, *FootMatchInfoRequest) (*Response, error)
 }
 
 // UnimplementedFootBallServer can be embedded to have forward compatible implementations.
 type UnimplementedFootBallServer struct {
 }
 
-func (*UnimplementedFootBallServer) MatchAgainstDetail(context.Context, *MatchInfoRequest) (*Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method MatchAgainstDetail not implemented")
+func (*UnimplementedFootBallServer) FootMatchAgainstDetail(context.Context, *FootMatchInfoRequest) (*Response, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method FootMatchAgainstDetail not implemented")
 }
 
 func RegisterFootBallServer(s *grpc.Server, srv FootBallServer) {
 	s.RegisterService(&_FootBall_serviceDesc, srv)
 }
 
-func _FootBall_MatchAgainstDetail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(MatchInfoRequest)
+func _FootBall_FootMatchAgainstDetail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(FootMatchInfoRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FootBallServer).MatchAgainstDetail(ctx, in)
+		return srv.(FootBallServer).FootMatchAgainstDetail(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/FootBall/MatchAgainstDetail",
+		FullMethod: "/FootBall/FootMatchAgainstDetail",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FootBallServer).MatchAgainstDetail(ctx, req.(*MatchInfoRequest))
+		return srv.(FootBallServer).FootMatchAgainstDetail(ctx, req.(*FootMatchInfoRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -381,8 +382,8 @@ var _FootBall_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*FootBallServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "MatchAgainstDetail",
-			Handler:    _FootBall_MatchAgainstDetail_Handler,
+			MethodName: "FootMatchAgainstDetail",
+			Handler:    _FootBall_FootMatchAgainstDetail_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
