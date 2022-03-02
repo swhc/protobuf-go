6cac8ab1a1fabb73b2eb72fe9621ebaabbc32e5b
root <root@localhost.localdomain>
2022-03-02 18:34:11 +0800

    root

--- abroad/device-server/collect_v2.pb.go
+++ abroad/device-server/collect_v2.pb.go
@@ -108,6 +108,77 @@ func (x *CollectRequestParam) GetSportId() int64 {
 	return 0
 }
 
+type CollectListRequestParam struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	UserId  int64 `protobuf:"varint,1,opt,name=UserId,proto3" json:"UserId,omitempty"`   //用户id
+	Type    int64 `protobuf:"varint,2,opt,name=Type,proto3" json:"Type,omitempty"`       // 数据类型 1：赛事，2：比赛，3：队伍，4：专家
+	IsGuest int64 `protobuf:"varint,5,opt,name=IsGuest,proto3" json:"IsGuest,omitempty"` //是否游客  0：否，1：是
+	SportId int64 `protobuf:"varint,6,opt,name=SportId,proto3" json:"SportId,omitempty"` //运动类型 1:足球，2：篮球
+}
+
+func (x *CollectListRequestParam) Reset() {
+	*x = CollectListRequestParam{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_collect_v2_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *CollectListRequestParam) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*CollectListRequestParam) ProtoMessage() {}
+
+func (x *CollectListRequestParam) ProtoReflect() protoreflect.Message {
+	mi := &file_collect_v2_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use CollectListRequestParam.ProtoReflect.Descriptor instead.
+func (*CollectListRequestParam) Descriptor() ([]byte, []int) {
+	return file_collect_v2_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *CollectListRequestParam) GetUserId() int64 {
+	if x != nil {
+		return x.UserId
+	}
+	return 0
+}
+
+func (x *CollectListRequestParam) GetType() int64 {
+	if x != nil {
+		return x.Type
+	}
+	return 0
+}
+
+func (x *CollectListRequestParam) GetIsGuest() int64 {
+	if x != nil {
+		return x.IsGuest
+	}
+	return 0
+}
+
+func (x *CollectListRequestParam) GetSportId() int64 {
+	if x != nil {
+		return x.SportId
+	}
+	return 0
+}
+
 var File_collect_v2_proto protoreflect.FileDescriptor
 
 var file_collect_v2_proto_rawDesc = []byte{
@@ -122,8 +193,16 @@ var file_collect_v2_proto_rawDesc = []byte{
 	0x28, 0x03, 0x52, 0x04, 0x54, 0x79, 0x70, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x49, 0x73, 0x47, 0x75,
 	0x65, 0x73, 0x74, 0x18, 0x05, 0x20, 0x01, 0x28, 0x03, 0x52, 0x07, 0x49, 0x73, 0x47, 0x75, 0x65,
 	0x73, 0x74, 0x12, 0x18, 0x0a, 0x07, 0x53, 0x70, 0x6f, 0x72, 0x74, 0x49, 0x64, 0x18, 0x06, 0x20,
-	0x01, 0x28, 0x03, 0x52, 0x07, 0x53, 0x70, 0x6f, 0x72, 0x74, 0x49, 0x64, 0x42, 0x07, 0x5a, 0x05,
-	0x2e, 0x2f, 0x3b, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x01, 0x28, 0x03, 0x52, 0x07, 0x53, 0x70, 0x6f, 0x72, 0x74, 0x49, 0x64, 0x22, 0x79, 0x0a, 0x17,
+	0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x4c, 0x69, 0x73, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65,
+	0x73, 0x74, 0x50, 0x61, 0x72, 0x61, 0x6d, 0x12, 0x16, 0x0a, 0x06, 0x55, 0x73, 0x65, 0x72, 0x49,
+	0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x06, 0x55, 0x73, 0x65, 0x72, 0x49, 0x64, 0x12,
+	0x12, 0x0a, 0x04, 0x54, 0x79, 0x70, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x03, 0x52, 0x04, 0x54,
+	0x79, 0x70, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x49, 0x73, 0x47, 0x75, 0x65, 0x73, 0x74, 0x18, 0x05,
+	0x20, 0x01, 0x28, 0x03, 0x52, 0x07, 0x49, 0x73, 0x47, 0x75, 0x65, 0x73, 0x74, 0x12, 0x18, 0x0a,
+	0x07, 0x53, 0x70, 0x6f, 0x72, 0x74, 0x49, 0x64, 0x18, 0x06, 0x20, 0x01, 0x28, 0x03, 0x52, 0x07,
+	0x53, 0x70, 0x6f, 0x72, 0x74, 0x49, 0x64, 0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2f, 0x3b, 0x76, 0x31,
+	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -138,9 +217,10 @@ func file_collect_v2_proto_rawDescGZIP() []byte {
 	return file_collect_v2_proto_rawDescData
 }
 
-var file_collect_v2_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_collect_v2_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_collect_v2_proto_goTypes = []interface{}{
-	(*CollectRequestParam)(nil), // 0: CollectRequestParam
+	(*CollectRequestParam)(nil),     // 0: CollectRequestParam
+	(*CollectListRequestParam)(nil), // 1: CollectListRequestParam
 }
 var file_collect_v2_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -168,6 +248,18 @@ func file_collect_v2_proto_init() {
 				return nil
 			}
 		}
+		file_collect_v2_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*CollectListRequestParam); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -175,7 +267,7 @@ func file_collect_v2_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_collect_v2_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
--- abroad/device-server/device_server.pb.go
+++ abroad/device-server/device_server.pb.go
@@ -62,6 +62,53 @@ func (*Empty) Descriptor() ([]byte, []int) {
 	return file_device_server_proto_rawDescGZIP(), []int{0}
 }
 
+type CollectList struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Ids []int64 `protobuf:"varint,1,rep,packed,name=Ids,proto3" json:"Ids,omitempty"` //数据列表
+}
+
+func (x *CollectList) Reset() {
+	*x = CollectList{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_device_server_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *CollectList) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*CollectList) ProtoMessage() {}
+
+func (x *CollectList) ProtoReflect() protoreflect.Message {
+	mi := &file_device_server_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use CollectList.ProtoReflect.Descriptor instead.
+func (*CollectList) Descriptor() ([]byte, []int) {
+	return file_device_server_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *CollectList) GetIds() []int64 {
+	if x != nil {
+		return x.Ids
+	}
+	return nil
+}
+
 var File_device_server_proto protoreflect.FileDescriptor
 
 var file_device_server_proto_rawDesc = []byte{
@@ -69,18 +116,24 @@ var file_device_server_proto_rawDesc = []byte{
 	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x1a, 0x13, 0x63, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x5f, 0x6d,
 	0x61, 0x74, 0x63, 0x68, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x1a, 0x10, 0x63, 0x6f, 0x6c, 0x6c,
 	0x65, 0x63, 0x74, 0x5f, 0x76, 0x32, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x07, 0x0a, 0x05,
-	0x45, 0x6d, 0x70, 0x74, 0x79, 0x32, 0x9d, 0x01, 0x0a, 0x06, 0x44, 0x65, 0x76, 0x69, 0x63, 0x65,
-	0x12, 0x31, 0x0a, 0x0c, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x4d, 0x61, 0x74, 0x63, 0x68,
-	0x12, 0x19, 0x2e, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x4d, 0x61, 0x74, 0x63, 0x68, 0x52,
-	0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x50, 0x61, 0x72, 0x61, 0x6d, 0x1a, 0x06, 0x2e, 0x45, 0x6d,
-	0x70, 0x74, 0x79, 0x12, 0x31, 0x0a, 0x0c, 0x50, 0x75, 0x73, 0x68, 0x4d, 0x73, 0x67, 0x4d, 0x61,
-	0x74, 0x63, 0x68, 0x12, 0x19, 0x2e, 0x50, 0x75, 0x73, 0x68, 0x4d, 0x73, 0x67, 0x4d, 0x61, 0x74,
-	0x63, 0x68, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x50, 0x61, 0x72, 0x61, 0x6d, 0x1a, 0x06,
-	0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x12, 0x2d, 0x0a, 0x0d, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63,
-	0x74, 0x48, 0x61, 0x6e, 0x64, 0x6c, 0x65, 0x12, 0x14, 0x2e, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63,
-	0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x50, 0x61, 0x72, 0x61, 0x6d, 0x1a, 0x06, 0x2e,
-	0x45, 0x6d, 0x70, 0x74, 0x79, 0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2f, 0x3b, 0x76, 0x31, 0x62, 0x06,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x45, 0x6d, 0x70, 0x74, 0x79, 0x22, 0x1f, 0x0a, 0x0b, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74,
+	0x4c, 0x69, 0x73, 0x74, 0x12, 0x10, 0x0a, 0x03, 0x49, 0x64, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28,
+	0x03, 0x52, 0x03, 0x49, 0x64, 0x73, 0x32, 0xd7, 0x01, 0x0a, 0x06, 0x44, 0x65, 0x76, 0x69, 0x63,
+	0x65, 0x12, 0x31, 0x0a, 0x0c, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x4d, 0x61, 0x74, 0x63,
+	0x68, 0x12, 0x19, 0x2e, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x4d, 0x61, 0x74, 0x63, 0x68,
+	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x50, 0x61, 0x72, 0x61, 0x6d, 0x1a, 0x06, 0x2e, 0x45,
+	0x6d, 0x70, 0x74, 0x79, 0x12, 0x31, 0x0a, 0x0c, 0x50, 0x75, 0x73, 0x68, 0x4d, 0x73, 0x67, 0x4d,
+	0x61, 0x74, 0x63, 0x68, 0x12, 0x19, 0x2e, 0x50, 0x75, 0x73, 0x68, 0x4d, 0x73, 0x67, 0x4d, 0x61,
+	0x74, 0x63, 0x68, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x50, 0x61, 0x72, 0x61, 0x6d, 0x1a,
+	0x06, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x12, 0x2d, 0x0a, 0x0d, 0x43, 0x6f, 0x6c, 0x6c, 0x65,
+	0x63, 0x74, 0x48, 0x61, 0x6e, 0x64, 0x6c, 0x65, 0x12, 0x14, 0x2e, 0x43, 0x6f, 0x6c, 0x6c, 0x65,
+	0x63, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x50, 0x61, 0x72, 0x61, 0x6d, 0x1a, 0x06,
+	0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x12, 0x38, 0x0a, 0x0e, 0x47, 0x65, 0x74, 0x43, 0x6f, 0x6c,
+	0x6c, 0x65, 0x63, 0x74, 0x4c, 0x69, 0x73, 0x74, 0x12, 0x18, 0x2e, 0x43, 0x6f, 0x6c, 0x6c, 0x65,
+	0x63, 0x74, 0x4c, 0x69, 0x73, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x50, 0x61, 0x72,
+	0x61, 0x6d, 0x1a, 0x0c, 0x2e, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x4c, 0x69, 0x73, 0x74,
+	0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2f, 0x3b, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x33,
 }
 
 var (
@@ -95,22 +148,26 @@ func file_device_server_proto_rawDescGZIP() []byte {
 	return file_device_server_proto_rawDescData
 }
 
-var file_device_server_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_device_server_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_device_server_proto_goTypes = []interface{}{
 	(*Empty)(nil),                    // 0: Empty
-	(*CollectMatchRequestParam)(nil), // 1: CollectMatchRequestParam
-	(*PushMsgMatchRequestParam)(nil), // 2: PushMsgMatchRequestParam
-	(*CollectRequestParam)(nil),      // 3: CollectRequestParam
+	(*CollectList)(nil),              // 1: CollectList
+	(*CollectMatchRequestParam)(nil), // 2: CollectMatchRequestParam
+	(*PushMsgMatchRequestParam)(nil), // 3: PushMsgMatchRequestParam
+	(*CollectRequestParam)(nil),      // 4: CollectRequestParam
+	(*CollectListRequestParam)(nil),  // 5: CollectListRequestParam
 }
 var file_device_server_proto_depIdxs = []int32{
-	1, // 0: Device.CollectMatch:input_type -> CollectMatchRequestParam
-	2, // 1: Device.PushMsgMatch:input_type -> PushMsgMatchRequestParam
-	3, // 2: Device.CollectHandle:input_type -> CollectRequestParam
-	0, // 3: Device.CollectMatch:output_type -> Empty
-	0, // 4: Device.PushMsgMatch:output_type -> Empty
-	0, // 5: Device.CollectHandle:output_type -> Empty
-	3, // [3:6] is the sub-list for method output_type
-	0, // [0:3] is the sub-list for method input_type
+	2, // 0: Device.CollectMatch:input_type -> CollectMatchRequestParam
+	3, // 1: Device.PushMsgMatch:input_type -> PushMsgMatchRequestParam
+	4, // 2: Device.CollectHandle:input_type -> CollectRequestParam
+	5, // 3: Device.GetCollectList:input_type -> CollectListRequestParam
+	0, // 4: Device.CollectMatch:output_type -> Empty
+	0, // 5: Device.PushMsgMatch:output_type -> Empty
+	0, // 6: Device.CollectHandle:output_type -> Empty
+	1, // 7: Device.GetCollectList:output_type -> CollectList
+	4, // [4:8] is the sub-list for method output_type
+	0, // [0:4] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -136,6 +193,18 @@ func file_device_server_proto_init() {
 				return nil
 			}
 		}
+		file_device_server_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*CollectList); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -143,7 +212,7 @@ func file_device_server_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_device_server_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
@@ -175,6 +244,7 @@ type DeviceClient interface {
 	PushMsgMatch(ctx context.Context, in *PushMsgMatchRequestParam, opts ...grpc.CallOption) (*Empty, error)
 	// 收藏、取消收藏操作
 	CollectHandle(ctx context.Context, in *CollectRequestParam, opts ...grpc.CallOption) (*Empty, error)
+	GetCollectList(ctx context.Context, in *CollectListRequestParam, opts ...grpc.CallOption) (*CollectList, error)
 }
 
 type deviceClient struct {
@@ -212,6 +282,15 @@ func (c *deviceClient) CollectHandle(ctx context.Context, in *CollectRequestPara
 	return out, nil
 }
 
+func (c *deviceClient) GetCollectList(ctx context.Context, in *CollectListRequestParam, opts ...grpc.CallOption) (*CollectList, error) {
+	out := new(CollectList)
+	err := c.cc.Invoke(ctx, "/Device/GetCollectList", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // DeviceServer is the server API for Device service.
 type DeviceServer interface {
 	// 收藏比赛 (取消也在)
@@ -220,6 +299,7 @@ type DeviceServer interface {
 	PushMsgMatch(context.Context, *PushMsgMatchRequestParam) (*Empty, error)
 	// 收藏、取消收藏操作
 	CollectHandle(context.Context, *CollectRequestParam) (*Empty, error)
+	GetCollectList(context.Context, *CollectListRequestParam) (*CollectList, error)
 }
 
 // UnimplementedDeviceServer can be embedded to have forward compatible implementations.
@@ -234,6 +314,9 @@ func (*UnimplementedDeviceServer) PushMsgMatch(context.Context, *PushMsgMatchReq
 }
 func (*UnimplementedDeviceServer) CollectHandle(context.Context, *CollectRequestParam) (*Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CollectHandle not implemented")
+}
+func (*UnimplementedDeviceServer) GetCollectList(context.Context, *CollectListRequestParam) (*CollectList, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetCollectList not implemented")
 }
 
 func RegisterDeviceServer(s *grpc.Server, srv DeviceServer) {
@@ -294,6 +377,24 @@ func _Device_CollectHandle_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Device_GetCollectList_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CollectListRequestParam)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DeviceServer).GetCollectList(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/Device/GetCollectList",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DeviceServer).GetCollectList(ctx, req.(*CollectListRequestParam))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Device_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "Device",
 	HandlerType: (*DeviceServer)(nil),
@@ -309,6 +410,10 @@ var _Device_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "CollectHandle",
 			Handler:    _Device_CollectHandle_Handler,
+		},
+		{
+			MethodName: "GetCollectList",
+			Handler:    _Device_GetCollectList_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
