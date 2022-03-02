180da84fff7e5414a405c56f3d6766e9b45e657c
root <root@localhost.localdomain>
2022-03-02 19:47:53 +0800

    root

--- abroad/device-server/collect_v2.pb.go
+++ abroad/device-server/collect_v2.pb.go
@@ -179,6 +179,77 @@ func (x *CollectListRequestParam) GetSportId() int64 {
 	return 0
 }
 
+type PushSettingRequestParam struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id      int64  `protobuf:"varint,1,opt,name=Id,proto3" json:"Id,omitempty"`           //用户id
+	Field   string `protobuf:"bytes,2,opt,name=Field,proto3" json:"Field,omitempty"`      // 要更新的字段
+	IsLogin int64  `protobuf:"varint,3,opt,name=IsLogin,proto3" json:"IsLogin,omitempty"` //是否游客  0：否，1：是
+	Value   string `protobuf:"bytes,4,opt,name=Value,proto3" json:"Value,omitempty"`      //要更新的值
+}
+
+func (x *PushSettingRequestParam) Reset() {
+	*x = PushSettingRequestParam{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_collect_v2_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PushSettingRequestParam) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PushSettingRequestParam) ProtoMessage() {}
+
+func (x *PushSettingRequestParam) ProtoReflect() protoreflect.Message {
+	mi := &file_collect_v2_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PushSettingRequestParam.ProtoReflect.Descriptor instead.
+func (*PushSettingRequestParam) Descriptor() ([]byte, []int) {
+	return file_collect_v2_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *PushSettingRequestParam) GetId() int64 {
+	if x != nil {
+		return x.Id
+	}
+	return 0
+}
+
+func (x *PushSettingRequestParam) GetField() string {
+	if x != nil {
+		return x.Field
+	}
+	return ""
+}
+
+func (x *PushSettingRequestParam) GetIsLogin() int64 {
+	if x != nil {
+		return x.IsLogin
+	}
+	return 0
+}
+
+func (x *PushSettingRequestParam) GetValue() string {
+	if x != nil {
+		return x.Value
+	}
+	return ""
+}
+
 var File_collect_v2_proto protoreflect.FileDescriptor
 
 var file_collect_v2_proto_rawDesc = []byte{
@@ -201,8 +272,15 @@ var file_collect_v2_proto_rawDesc = []byte{
 	0x79, 0x70, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x49, 0x73, 0x47, 0x75, 0x65, 0x73, 0x74, 0x18, 0x05,
 	0x20, 0x01, 0x28, 0x03, 0x52, 0x07, 0x49, 0x73, 0x47, 0x75, 0x65, 0x73, 0x74, 0x12, 0x18, 0x0a,
 	0x07, 0x53, 0x70, 0x6f, 0x72, 0x74, 0x49, 0x64, 0x18, 0x06, 0x20, 0x01, 0x28, 0x03, 0x52, 0x07,
-	0x53, 0x70, 0x6f, 0x72, 0x74, 0x49, 0x64, 0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2f, 0x3b, 0x76, 0x31,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x53, 0x70, 0x6f, 0x72, 0x74, 0x49, 0x64, 0x22, 0x6f, 0x0a, 0x17, 0x50, 0x75, 0x73, 0x68, 0x53,
+	0x65, 0x74, 0x74, 0x69, 0x6e, 0x67, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x50, 0x61, 0x72,
+	0x61, 0x6d, 0x12, 0x0e, 0x0a, 0x02, 0x49, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x02,
+	0x49, 0x64, 0x12, 0x14, 0x0a, 0x05, 0x46, 0x69, 0x65, 0x6c, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28,
+	0x09, 0x52, 0x05, 0x46, 0x69, 0x65, 0x6c, 0x64, 0x12, 0x18, 0x0a, 0x07, 0x49, 0x73, 0x4c, 0x6f,
+	0x67, 0x69, 0x6e, 0x18, 0x03, 0x20, 0x01, 0x28, 0x03, 0x52, 0x07, 0x49, 0x73, 0x4c, 0x6f, 0x67,
+	0x69, 0x6e, 0x12, 0x14, 0x0a, 0x05, 0x56, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x04, 0x20, 0x01, 0x28,
+	0x09, 0x52, 0x05, 0x56, 0x61, 0x6c, 0x75, 0x65, 0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2f, 0x3b, 0x76,
+	0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -217,10 +295,11 @@ func file_collect_v2_proto_rawDescGZIP() []byte {
 	return file_collect_v2_proto_rawDescData
 }
 
-var file_collect_v2_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_collect_v2_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_collect_v2_proto_goTypes = []interface{}{
 	(*CollectRequestParam)(nil),     // 0: CollectRequestParam
 	(*CollectListRequestParam)(nil), // 1: CollectListRequestParam
+	(*PushSettingRequestParam)(nil), // 2: PushSettingRequestParam
 }
 var file_collect_v2_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -260,6 +339,18 @@ func file_collect_v2_proto_init() {
 				return nil
 			}
 		}
+		file_collect_v2_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PushSettingRequestParam); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -267,7 +358,7 @@ func file_collect_v2_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_collect_v2_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
--- abroad/device-server/device_server.pb.go
+++ abroad/device-server/device_server.pb.go
@@ -118,7 +118,7 @@ var file_device_server_proto_rawDesc = []byte{
 	0x65, 0x63, 0x74, 0x5f, 0x76, 0x32, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x07, 0x0a, 0x05,
 	0x45, 0x6d, 0x70, 0x74, 0x79, 0x22, 0x1f, 0x0a, 0x0b, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74,
 	0x4c, 0x69, 0x73, 0x74, 0x12, 0x10, 0x0a, 0x03, 0x49, 0x64, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28,
-	0x03, 0x52, 0x03, 0x49, 0x64, 0x73, 0x32, 0xd7, 0x01, 0x0a, 0x06, 0x44, 0x65, 0x76, 0x69, 0x63,
+	0x03, 0x52, 0x03, 0x49, 0x64, 0x73, 0x32, 0x8e, 0x02, 0x0a, 0x06, 0x44, 0x65, 0x76, 0x69, 0x63,
 	0x65, 0x12, 0x31, 0x0a, 0x0c, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x4d, 0x61, 0x74, 0x63,
 	0x68, 0x12, 0x19, 0x2e, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x4d, 0x61, 0x74, 0x63, 0x68,
 	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x50, 0x61, 0x72, 0x61, 0x6d, 0x1a, 0x06, 0x2e, 0x45,
@@ -132,8 +132,11 @@ var file_device_server_proto_rawDesc = []byte{
 	0x6c, 0x65, 0x63, 0x74, 0x4c, 0x69, 0x73, 0x74, 0x12, 0x18, 0x2e, 0x43, 0x6f, 0x6c, 0x6c, 0x65,
 	0x63, 0x74, 0x4c, 0x69, 0x73, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x50, 0x61, 0x72,
 	0x61, 0x6d, 0x1a, 0x0c, 0x2e, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x4c, 0x69, 0x73, 0x74,
-	0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2f, 0x3b, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x33,
+	0x12, 0x35, 0x0a, 0x11, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x50, 0x75, 0x73, 0x68, 0x53, 0x65,
+	0x74, 0x74, 0x69, 0x6e, 0x67, 0x12, 0x18, 0x2e, 0x50, 0x75, 0x73, 0x68, 0x53, 0x65, 0x74, 0x74,
+	0x69, 0x6e, 0x67, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x50, 0x61, 0x72, 0x61, 0x6d, 0x1a,
+	0x06, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2f, 0x3b, 0x76, 0x31,
+	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -156,18 +159,21 @@ var file_device_server_proto_goTypes = []interface{}{
 	(*PushMsgMatchRequestParam)(nil), // 3: PushMsgMatchRequestParam
 	(*CollectRequestParam)(nil),      // 4: CollectRequestParam
 	(*CollectListRequestParam)(nil),  // 5: CollectListRequestParam
+	(*PushSettingRequestParam)(nil),  // 6: PushSettingRequestParam
 }
 var file_device_server_proto_depIdxs = []int32{
 	2, // 0: Device.CollectMatch:input_type -> CollectMatchRequestParam
 	3, // 1: Device.PushMsgMatch:input_type -> PushMsgMatchRequestParam
 	4, // 2: Device.CollectHandle:input_type -> CollectRequestParam
 	5, // 3: Device.GetCollectList:input_type -> CollectListRequestParam
-	0, // 4: Device.CollectMatch:output_type -> Empty
-	0, // 5: Device.PushMsgMatch:output_type -> Empty
-	0, // 6: Device.CollectHandle:output_type -> Empty
-	1, // 7: Device.GetCollectList:output_type -> CollectList
-	4, // [4:8] is the sub-list for method output_type
-	0, // [0:4] is the sub-list for method input_type
+	6, // 4: Device.UpdatePushSetting:input_type -> PushSettingRequestParam
+	0, // 5: Device.CollectMatch:output_type -> Empty
+	0, // 6: Device.PushMsgMatch:output_type -> Empty
+	0, // 7: Device.CollectHandle:output_type -> Empty
+	1, // 8: Device.GetCollectList:output_type -> CollectList
+	0, // 9: Device.UpdatePushSetting:output_type -> Empty
+	5, // [5:10] is the sub-list for method output_type
+	0, // [0:5] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -245,6 +251,7 @@ type DeviceClient interface {
 	// 收藏、取消收藏操作
 	CollectHandle(ctx context.Context, in *CollectRequestParam, opts ...grpc.CallOption) (*Empty, error)
 	GetCollectList(ctx context.Context, in *CollectListRequestParam, opts ...grpc.CallOption) (*CollectList, error)
+	UpdatePushSetting(ctx context.Context, in *PushSettingRequestParam, opts ...grpc.CallOption) (*Empty, error)
 }
 
 type deviceClient struct {
@@ -291,6 +298,15 @@ func (c *deviceClient) GetCollectList(ctx context.Context, in *CollectListReques
 	return out, nil
 }
 
+func (c *deviceClient) UpdatePushSetting(ctx context.Context, in *PushSettingRequestParam, opts ...grpc.CallOption) (*Empty, error) {
+	out := new(Empty)
+	err := c.cc.Invoke(ctx, "/Device/UpdatePushSetting", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // DeviceServer is the server API for Device service.
 type DeviceServer interface {
 	// 收藏比赛 (取消也在)
@@ -300,6 +316,7 @@ type DeviceServer interface {
 	// 收藏、取消收藏操作
 	CollectHandle(context.Context, *CollectRequestParam) (*Empty, error)
 	GetCollectList(context.Context, *CollectListRequestParam) (*CollectList, error)
+	UpdatePushSetting(context.Context, *PushSettingRequestParam) (*Empty, error)
 }
 
 // UnimplementedDeviceServer can be embedded to have forward compatible implementations.
@@ -317,6 +334,9 @@ func (*UnimplementedDeviceServer) CollectHandle(context.Context, *CollectRequest
 }
 func (*UnimplementedDeviceServer) GetCollectList(context.Context, *CollectListRequestParam) (*CollectList, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetCollectList not implemented")
+}
+func (*UnimplementedDeviceServer) UpdatePushSetting(context.Context, *PushSettingRequestParam) (*Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdatePushSetting not implemented")
 }
 
 func RegisterDeviceServer(s *grpc.Server, srv DeviceServer) {
@@ -395,6 +415,24 @@ func _Device_GetCollectList_Handler(srv interface{}, ctx context.Context, dec fu
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Device_UpdatePushSetting_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(PushSettingRequestParam)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DeviceServer).UpdatePushSetting(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/Device/UpdatePushSetting",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DeviceServer).UpdatePushSetting(ctx, req.(*PushSettingRequestParam))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Device_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "Device",
 	HandlerType: (*DeviceServer)(nil),
@@ -414,6 +452,10 @@ var _Device_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetCollectList",
 			Handler:    _Device_GetCollectList_Handler,
+		},
+		{
+			MethodName: "UpdatePushSetting",
+			Handler:    _Device_UpdatePushSetting_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
